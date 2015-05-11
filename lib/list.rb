1b5b6eb42e80c023b497292da39004e3c685a005
jessica engel & cody bouscaren <student@epicodus.com>
2015-05-11 10:06:48 -0700

    return all list

--- lib/list.rb
+++ lib/list.rb
@@ -7,6 +7,30 @@ class List
     @name = attributes.fetch(:name)
   end
 
-  
+  define_method(:save) do
+    result= DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
+
+  define_method(:==) do |other_list|
+    self.name == other_list.name
+  end
+
+  define_singleton_method(:all) do
+    result= DB.exec("SELECT * FROM lists;")
+    arr = []
+    result.each do |object|
+    name = object.fetch("name")
+    id = object.fetch("id").to_i
+    arr.push(List.new({:name => name, :id => id}))
+  end
+  arr
+  end
+
+
+
+
+
+
 
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -8,4 +8,28 @@ describe("List") do
       expect(test_list.name).to(eq("Tony"))
     end
   end
+
+  describe("#id") do
+    it("returns the id of the list") do
+      test_list = List.new({:name => "Tony", :id => nil})
+      test_list.save()
+      expect(test_list.id).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe(".save") do
+    it("saves the list in database") do
+      test_list = List.new({:name => "Barb", :id => nil})
+      test_list.save()
+      expect(List.all()).to(eq([test_list]))
+    end
+  end
+
+
+
+
+
+
+
+
 end
